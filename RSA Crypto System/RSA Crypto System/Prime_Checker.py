763f0b5c08c587fffc7ad1c5094d0831ababc911
Brythe40 <113539993+Brythe40@users.noreply.github.com>
2024-04-19 18:07:08 -0500

    step 3

--- RSA Crypto System/RSA Crypto System/Prime_Checker.py
+++ RSA Crypto System/RSA Crypto System/Prime_Checker.py
@@ -1,15 +1,54 @@
-class prime_checker:
-    def check_prime(self, number, iterator):
+from tokenize import Number
+
+
+class Prime_Checker:
+    # check if number is prime, boolean
+    def check_prime(number):
         # corner case
-        if(number == 0 or number ==1): 
+        if(number <=1): 
             return False
         # check prime
-        if(number == iterator): 
+        if(number <= 3): 
             return True
         # base case
-        if(number % iterator == 0): 
+        if(number % 2 == 0 or number % 3 == 0): 
             return False
-        iterator += 1
+        i = 5
+        while i * i <= number:
+            if number % i == 0 or number % (i + 2) == 0:
+                return False
+            i += 6
+        return True
 
         return self.check_prime(number, iterator)
+
+    # get 10th (p) and 19th (q) prime numbers between 1,000 and 10,000
+    def get_primes():
+        primes = []
+        number = 1000
+        while len(primes) < 19:
+            if Prime_Checker.check_prime(number):
+                primes.append(number)
+            number += 1
+        return primes
+
+    # get the inverse modulous
+    def inverse_mod(e, phi):
+        original_e = e
+        y = 0
+        x = 1
+
+        if e == 1: return 0
+
+        while e > 1:
+            q = e // phi
+            temp = phi
+            phi = e % phi
+            e = temp
+            temp = y
+            y = x - q * y
+            x = temp
+
+        if x < 0: x = x + original_e
+        return x
     pass
--- RSA Crypto System/RSA Crypto System/RSA_Crypto_System.py
+++ RSA Crypto System/RSA Crypto System/RSA_Crypto_System.py
@@ -1,11 +1,23 @@
 # Mobile Security and Cryptography Problem 2
 # RSA Crypto System
 # Bryce Olivier, Robert Spampneto, Harrison Bourg, and Brennan Butler
+from Prime_Checker import Prime_Checker
 
-from Prime_Checker import prime_checker
+
+#print("Enter a number: ")
+#num = int(input())
+
+#pchecker = Prime_Checker.check_prime(num)
+primes = Prime_Checker.get_primes()
+p = primes[9]
+q = primes[18]
+n = p * q
+e = 65537
+d = Prime_Checker.inverse_mod(e, (p - 1) * ( q - 1))
+
+public_key = {'e': e, 'n': n}
+private_key = {'d': d, 'p': p, 'q': q}
 
 
-print("Enter a number: ")
-num = int(input())
-pchecker = prime_checker().check_prime(num, 2)
-print(str(num) + "'s prime value is: " + str(pchecker))
+print("public: ", public_key)
+print("private: ", private_key)

54013d3a998b052fdc153a2da5f7ce3bbda95bbf
Brythe40 <113539993+Brythe40@users.noreply.github.com>
2024-04-19 19:57:23 -0500

    almost done

--- RSA Crypto System/RSA Crypto System/Prime_Checker.py
+++ RSA Crypto System/RSA Crypto System/Prime_Checker.py
@@ -1,4 +1,5 @@
 from tokenize import Number
+import time
 
 
 class Prime_Checker:
@@ -23,9 +24,8 @@ class Prime_Checker:
         return self.check_prime(number, iterator)
 
     # get 10th (p) and 19th (q) prime numbers between 1,000 and 10,000
-    def get_primes():
+    def get_primes(number):
         primes = []
-        number = 1000
         while len(primes) < 19:
             if Prime_Checker.check_prime(number):
                 primes.append(number)
@@ -51,4 +51,33 @@ class Prime_Checker:
 
         if x < 0: x = x + original_e
         return x
+
+    def encrypt(message, key):
+        e = key['e']
+        n = key['n']
+        encrypted = [(ord(char) - ord('a'))**e % n for char in message]
+        return encrypted
+
+    def decrypt(message, key):
+        d = key['d']
+        p = key['p']
+        q = key['q']
+        n = p * q
+        decrypted = [chr((char**d) % n + ord('a')) for char in message]
+        return ''.join(decrypted)
+
+    def exhaustive_search(key):
+        e = key['e']
+        n = key['n']
+        start_time = time.time()
+        d = 2
+        primes = Prime_Checker.get_primes(n)
+        p = primes[9]
+        q = primes[18]
+        while True:
+            if (e * d) % ((p - 1) * (q - 1)) == 1:
+                break
+            d += 1
+        end_time = time.time()
+        print("Exhuastive search found private key (d): ", d, " in ", end_time - start_time, "seconds")
     pass
--- RSA Crypto System/RSA Crypto System/RSA_Crypto_System.py
+++ RSA Crypto System/RSA Crypto System/RSA_Crypto_System.py
@@ -8,9 +8,12 @@ from Prime_Checker import Prime_Checker
 #num = int(input())
 
 #pchecker = Prime_Checker.check_prime(num)
-primes = Prime_Checker.get_primes()
+primes = Prime_Checker.get_primes(1000)
 p = primes[9]
 q = primes[18]
+
+print("10th and 19th prime numbers: ", p, q)
+
 n = p * q
 e = 65537
 d = Prime_Checker.inverse_mod(e, (p - 1) * ( q - 1))
@@ -18,6 +21,10 @@ d = Prime_Checker.inverse_mod(e, (p - 1) * ( q - 1))
 public_key = {'e': e, 'n': n}
 private_key = {'d': d, 'p': p, 'q': q}
 
+message = "rsa"
+encrpt = Prime_Checker.encrypt(message, public_key)
+print("Encrypted message: ", encrpt)
+decrpt = Prime_Checker.decrypt(encrpt, private_key)
+print("Decrypted message: ", decrpt)
 
-print("public: ", public_key)
-print("private: ", private_key)
+Prime_Checker.exhaustive_search(public_key)
